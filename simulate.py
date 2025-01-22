4d450dc7600e86bdc0a777eba388bc47096399df
Jackson Swalley <jacksonswalley@gmail.com>
2025-01-22 15:40:52 -0500

    Starting work on simulation.

--- simulate.py
+++ simulate.py
@@ -1,2 +1,16 @@
 
 # evolutionary robotics HW1
+import pybullet as p
+import time
+
+physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+count = 0
+for _ in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    count+=1
+    print(count)
+
+p.disconnect()
