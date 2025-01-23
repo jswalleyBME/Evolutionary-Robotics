616b9e35dd001a836d812d984a6ac35671ff0d84
Jackson Swalley <jacksonswalley@gmail.com>
2025-01-22 19:23:18 -0500

    update

--- simulate.py
+++ simulate.py
@@ -1,13 +1,20 @@
 
 # evolutionary robotics HW1
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # location of .urdf
+p.setGravity(0,0,-9.8)                                  # gravity
+planeId = p.loadURDF("plane.urdf")                      # set floor 
+p.loadSDF("box.sdf")                                    
 
 count = 0
 for _ in range(1000):
+    
+    # step simulation and time delay 
     p.stepSimulation()
     time.sleep(1/60)
     count+=1
