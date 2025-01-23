423ea59f234f8f863e533f776a9632aa7821c68e
Jackson Swalley <jacksonswalley@gmail.com>
2025-01-23 18:01:35 -0500

    save

--- simulate.py
+++ simulate.py
@@ -2,7 +2,9 @@
 # evolutionary robotics HW1
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
+import numpy as np
 
 physicsClient = p.connect(p.GUI)
 
@@ -12,13 +14,22 @@ planeId = p.loadURDF("plane.urdf")                      # set floor
 robotId = p.loadURDF("body.urdf")   
 p.loadSDF("world.sdf")                                    
 
+
 count = 0
-for _ in range(10000):
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
+pyrosim.Prepare_To_Simulate(robotId)
+for n in range(1000):
 
     # step simulation and time delay 
     p.stepSimulation()
+    
+    backLegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    
     time.sleep(1/60)
     count+=1
-    print(count)
+np.save("data/back_leg_sensor", backLegSensorValues)
+np.save("data/front_leg_sensor", frontLegSensorValues)
 
 p.disconnect()
