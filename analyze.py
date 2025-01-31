2aac2fb2d88e81cada5395d2837e2f6cc112583a
Jackson Swalley <jacksonswalley@gmail.com>
2025-01-31 11:39:51 -0500

    save hw5

--- analyze.py
+++ analyze.py
@@ -4,11 +4,12 @@ import numpy as np
 
 data_back = np.load("data/back_leg_sensor.npy")
 data_front = np.load("data/front_leg_sensor.npy")
+data_sin = np.load("data/sin_wave")
 
-plt.figure()
-plt.plot(data_back, label = "Back Leg")
-plt.plot(data_front, label = "Front Leg")
-plt.xlabel("step")
-plt.ylabel("value")
-plt.legend()
+plt.figure(1)
+#plt.plot(data_back, label = "Back Leg")
+#plt.plot(data_front, label = "Front Leg")
+#plt.xlabel("step")
+#plt.ylabel("value")
+plt.plot(data_sin)
 plt.show()
